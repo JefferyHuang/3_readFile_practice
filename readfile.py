787a189e34bc34d81ba594021498db6946fcd940
JefferyHuang <107784099+JefferyHuang@users.noreply.github.com>
2022-06-28 20:21:55 +0800

    Add files via upload

--- readfile.py
+++ readfile.py
@@ -1,5 +1,3 @@
-from this import d
-
 
 data = []
 count = 0
@@ -16,3 +14,22 @@ for l in data:
     sumLen = sumLen + len(l)
 
 print("留言的平均長度為", sumLen/len(data))
+
+new = []
+for l in data:
+    if len(l) < 100:
+        new.append(l)
+print("總共有", len(new), "筆資料長度小於100")
+print(new[0])
+
+good = []
+for l in data:
+    if "good" in l:
+        good.append(l)
+print("總共有", len(good), "筆資料提到good")
+print(good[2])
+
+#快寫法
+good = [l for l in data if good in l] #印出含有good的清單
+
+bad = ["bad" in l for l in data] #印出 True 或 Flase
